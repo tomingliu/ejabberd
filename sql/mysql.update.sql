9ca531b70e439f2c3312f3d019c0e5bc19683c3a
刘明 <lium@wiseuc.com>
2018-01-17 14:36:40 +0800

    Add trigger function to set `userid` automatically

--- sql/mysql.update.sql
+++ sql/mysql.update.sql
@@ -1,4 +1,4 @@
-alter table users add userid varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
+ALTER TABLE users ADD userid VARCHAR(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
 
 DELIMITER ;;
 CREATE TRIGGER before_add_user
