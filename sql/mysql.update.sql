e2e271f89c3cb0b9648895ce3c10aa930e577ac3
刘明 <lium@wiseuc.com>
2018-01-18 10:09:45 +0800

    Enable function creation

--- sql/mysql.update.sql
+++ sql/mysql.update.sql
@@ -1,5 +1,7 @@
 ALTER TABLE users ADD userid VARCHAR(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
 
+SET GLOBAL log_bin_trust_function_creators = 1;
+
 DELIMITER ;;
 CREATE TRIGGER before_add_user
 BEFORE INSERT ON users
